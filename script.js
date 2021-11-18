3df517dfd294f5b576ee100317aca235b07da5b8
gabriel <gabriel@gilly.st>
2021-11-18 16:33:31 +0000

    grid size definition working

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
+// Default
+
 // Create the divs
     // Get the node where to add the divs
 const container = document.getElementById('container');
+const pixels = document.querySelectorAll('.pixel'); // capture all pixel if already existing
+
 console.log(container);
     // Create the divs
-for(i=1; i<=256; i++){
-    let pixel = document.createElement("div");
-    pixel.id = `pixel${i}`
-    pixel.classList = "pixel"
-    container.appendChild(pixel);
-    console.log(`div${i} was added to ${container}`)
-};
-console.log(`a total of ${i-1} div have been added`);
+    // Wrap the grid creation into a function
+function gridCreation(size){
+    for(i=1; i<=size*size; i++){
+        let pixel = document.createElement("div"); // create element
+        pixel.id = `pixel-${i}` // add id
+        pixel.classList = "pixel" // add class
+            // define size based on the grid size
+        pixel.style.width= parseInt(500/size)+'px';
+        pixel.style.height=parseInt(500/size)+ 'px';
+        container.appendChild(pixel); // add to html
+            //add event listner
+        pixel.addEventListener('mouseenter', (e)=>{
+            console.log(`hover ${pixel.id}`);
+            pixel.classList.add("active");
+        });
+    };
+        // select all pixel into anodelist for use in other functions
+    const pixels = document.querySelectorAll('.pixel');
+    console.log(`a total of ${i-1} div have been added`);
+}
 
-// Implement event listener in the divs
-    // get all pixels into a nodelist
-const pixels = document.querySelectorAll('.pixel');
-    // loop for each and add an eventlisnter
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseenter', (e)=>{
-        console.log(pixel);
-        pixel.classList.add("active");
-    });
-    console.log(`eventlistener added for ${pixel}`)
-});
 
 // Change color when hover
     // Function that change the color when requested
@@ -32,26 +37,33 @@ function changeColor(pixel){
 
 // Clear all div to default color
 function clearGrid() {
+    const pixels = document.querySelectorAll('.pixel');
     pixels.forEach(pixel =>{
         pixel.classList.remove("active");
     });
+    console.log('grid cleared');
 };
 
 // Set size of the etching grid
     // promt the user to enter his grid size
 function gridSizePrompt(){
     gridSize = prompt('What size would you like?');
+    if (prompt)
     console.log(`return:${gridSize}`);
     deleteGrid();
+    gridCreation(gridSize);
 }
     //function to delete all pixels
 function deleteGrid(){
     pixels.forEach(pixel=>{
-        pixel.remove();
-        console.log(`remove ${pixel}`);
+        pixel.remove;
     })
+    console.log('deleted all pixels');
 }
 
 // Add a ramdom efect to the color
 
 // increase the intensity of the color slowly
+
+// initialisation
+gridCreation(16);
